479a04a2329f0a04e6107f3a4126a23262e44f2f
Jesse Sheehan <jesse@sheehan.nz>
2018-10-22 19:56:52 +1300

    Changed the message to make sure deployment works.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = new express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, World");
+  res.send("Hello, World 2");
 });
 
 app.listen(process.env.PORT || 3000, () => {
