76afe363ebc9143792bee931bd4b2cf576a31918
Jesse Sheehan <jesse@sheehan.nz>
2018-10-22 17:21:38 +1300

    Disabled everything. Trying to figure out why IIS won't work.

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,26 @@
 import express from "express";
-import express_graphql from "express-graphql";
-import { buildSchema } from "graphql";
-import { Connection } from "tedious";
-import jwt from "express-jwt";
-import dotenv from "dotenv";
+// import express_graphql from "express-graphql";
+// import { buildSchema } from "graphql";
+// import { Connection } from "tedious";
+// import jwt from "express-jwt";
+// import dotenv from "dotenv";
 
 // configure the environment variables
-dotenv.config();
+// dotenv.config();
 
 // configure the port number to run on
 const port = process.env.PORT || 3000;
 
 // configure the database
-const db_config = {
-  userName: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  server: process.env.DB_HOST,
-  options: {
-    database: process.env.DB_NAME,
-    encrypt: true
-  }
-};
+// const db_config = {
+//   userName: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   server: process.env.DB_HOST,
+//   options: {
+//     database: process.env.DB_NAME,
+//     encrypt: true
+//   }
+// };
 
 // configure the database
 // const conn = new Connection(db_config);
@@ -34,30 +34,30 @@ const db_config = {
 // });
 
 // configure authentication
-const auth = jwt({
-  secret: process.env.JWT_SECRET || ""
-});
+// const auth = jwt({
+//   secret: process.env.JWT_SECRET || ""
+// });
 
-// schema
-const schema = buildSchema(`
-  type Query {
-    message: String
-  }
-`);
+// // schema
+// const schema = buildSchema(`
+//   type Query {
+//     message: String
+//   }
+// `);
 
 // root resolver
-const root = {
-  message: () => 'Hello, World!'
-};
+// const root = {
+//   message: () => 'Hello, World!'
+// };
 
 const app = express();
 
-// setup the graphql api with authentication
-app.use('/api/graphql', auth, express_graphql({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}));
+// // setup the graphql api with authentication
+// app.use('/api/graphql', auth, express_graphql({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true
+// }));
 
 app.use((req: express.Request, res: express.Response) => {
   res.send("Under Construction");
