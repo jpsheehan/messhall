9ec16fc79b54df9a037184bed3861874311cd3f8
Jesse Sheehan <jesse@sheehan.nz>
2018-10-22 18:02:47 +1300

    Something smple...

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-require("./bin/server.js");
+// require("./bin/server.js");
+
+const express = require("express");
+const app = new express();
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+app.listen(process.env.PORT || 3000);
