fa90262db99c40baf366701068cbc76b3e5e4da4
Jesse Sheehan <jesse@sheehan.nz>
2018-10-22 18:25:06 +1300

    Getting something simple working for Azure.

--- app.js
+++
@@ -1,8 +0,0 @@
-// require("./bin/server.js");
-
-const express = require("express");
-const app = new express();
-app.get("/", (req, res) => {
-  res.send("Hi");
-});
-app.listen(process.env.port || 3000);
