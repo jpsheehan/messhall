73c49eb02bbd0fa0811bb8241e3ffca0150974a9
Jesse Sheehan <jesse@sheehan.nz>
2018-10-23 21:14:21 +1300

    Got it to work. Outputting some version information

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = new express();
 
 app.get("/", function(req, res) {
-  res.send("Hello, World 2");
+  res.send("Hello, World 2" + process.version);
 });
 
 app.listen(process.env.PORT || 3000, function() {
