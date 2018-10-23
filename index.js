755e7cf413909926eb70aac64995cd88bd204192
Jesse Sheehan <jesse@sheehan.nz>
2018-10-23 21:11:36 +1300

    Changed arrow function syntax to older style function syntax.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = new express();
 
-app.get("/", (req, res) => {
+app.get("/", function(req, res) {
   res.send("Hello, World 2");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening");
 });
