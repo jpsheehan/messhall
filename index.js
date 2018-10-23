d87ae3e10ee06eabf46bd7f2339966df5ca33663
Jesse Sheehan <jesse@sheehan.nz>
2018-10-24 09:31:37 +1300

    Added dependencies. Setup eslint

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = new express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", function(req, res) {
   res.send("Hello, World 2" + process.version);
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Listening");
+app.listen(port, function() {
+  console.log(`Express: http://localhost:${port}/`);
+  // console.log(`GraphQL: http://localhost:${port}/graphql`);
 });
