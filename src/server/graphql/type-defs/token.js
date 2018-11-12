7ae3e12b5bfa32dfce43f12a8fbb65d8ab71e0cd
Jesse Sheehan <jesse@sheehan.nz>
2018-11-13 09:55:03 +1300

    Documented the User and Token related types, inputs, queries and mutations

--- src/server/graphql/type-defs/token.js
+++ src/server/graphql/type-defs/token.js
@@ -1,38 +1,93 @@
 export default {
   types: `
+    # The Token object belongs to one user and represents a login session. A
+    # user can be logged in via the createToken mutation and logged out via the
+    # deleteToken mutation.
     type Token {
+
+      # The unique ID of the Token. Use this when using queries and mutations
+      # that require a Token ID.
       id: Int!
+
+      # A unique ID that references this Token. Considering removing this from
+      # future releases.
       uuid: String!
+
+      # The User that the Token belongs to.
       user: User!
     }
     
+    # Contains the User returned after the token is deleted. This could be
+    # useful for the client to know.
     type DeleteTokenPayload {
+
+      # The User that the deleted Token belonged to.
       user: User
     }
 
+    # Contains the User, Token and authToken objects. This is returned from the
+    # createToken mutation.<br />
+    # The authToken is a string
+    # that contains the token that should be inserted into the "Authorization"
+    # header for every subsequent request as follows:
+    # <pre>Authorization: Bearer &lt;authToken&gt;</pre>
     type CreateTokenPayload {
+
+      # The User that this Token belongs to and that has been "signed in".
       user: User
+
+      # The bearer token string to be inserted into the authorization header
+      # as described above.
       authToken: String
+
+      # The actual Token object. It may be useful for the client to store the
+      # id property for the purposes of signing out (the deleteToken mutation).
       token: Token
     }
   `,
   queries: `
+    
+    # Requires Admin role. Gets a list of all active Tokens in the system.
     tokens: [Token]
+
+    # Requires Admin role. Gets any Token in the system by the Token ID.<br />
+    # Requires any role. Gets any Token in the system that belongs to the
+    # currently signed in User.
     token(id: Int!): Token
   `,
   mutations: `
+    
+    # Creates a Token for a User. This is used to "sign in" a User.
     createToken(input: CreateToken!): CreateTokenPayload
+
+    # Creates a Token for a manager or admin User only. This is used to
+    # "sign in" a User. If a user attempts to run this mutation an error will
+    # be returned.
     createSuperToken(input: CreateToken!): CreateTokenPayload
+
+    # Deletes a Token from the system. The Token must belong to the currently
+    # signed in User.
     deleteToken(input: DeleteToken!): DeleteTokenPayload
   `,
   inputs: `
+    
+    # Used to create a new Token, or sign a User in.
     input CreateToken {
+
+      # The email address of the User that wishes to sign in.
       email: String
+
+      # The password of the User that wishes to sign in.
       password: String
+      
     }
 
+    # Used to delete an exisiting Token from the system.
     input DeleteToken {
+
+      # The ID of the Token to be deleted.
       id: Int!
+
     }
   `,
 };
--- src/server/graphql/type-defs/user.js
+++ src/server/graphql/type-defs/user.js
@@ -1,54 +1,129 @@
 export default {
   types: `
+
+    # A User is any user, manager or administrator that has access to this
+    # system. A User can access the system by the phone app (user) or by the
+    # web portal (administrators/managers).
     type User {
+
+      # A unique ID that identifies the User in the system. This will be
+      # required for any queries or mutations that need an ID for the User.
       id: Int!
+
+      # The first name of the User.
       firstName: String!
+
+      # The last name of the User.
       lastName: String!
+
+      # The full name of the User. This is simply a concatenation of the
+      # firstName and lastName properties.
       name: String!
+
+      # The email address of the User. This may be used to send the user
+      # password reset emails and such.
       email: String!
+
+      # Must be one of 'user', 'manager' or 'admin'. Depending on what kind of
+      # permissions the User requires.
       role: String!
+
+      # Contains a list of all the active Tokens the User has. Deleting all of
+      # these would effectively sign a User out of all sessions.
       tokens: [Token]!
+
+      # The current number of points that a User has to spend.
       points: Int!
+
+      # A total accounting of all the User's redemptions and attendances.
       history: [History]!
+
     }
 
+    # Returned from the createUser mutation.
     type CreateUserPayload {
+
+      # The User that was just created.
       user: User
+
     }
   
+    # Returned from the deleteUser mutation.
     type DeleteUserPayload {
+
+      # The User that was just deleted.
       user: User
+
     }
   
+    # Returned from the updateUser mutation
     type UpdateUserPayload {
+
+      # The updated User.
       user: User
+
     }
   `,
   queries: `
+    
+    # Requires admin. Returns a list of all Users in the system.
     users: [User]
+
+    # Requires admin. Returns a single User based on ID.<br />
+    # Requires user/manager. Returns the signed in User based on ID.
     user(id: Int!): User
   `,
   mutations: `
+    
+    # Requires admin. Creates a new User in the system.
     createUser(input: CreateUser!): CreateUserPayload
+
+    # Requires admin. Updates an existing User.
     updateUser(input: UpdateUser!): UpdateUserPayload
+
+    # Requires admin. Deletes an existing User.
     deleteUser(input: DeleteUser!): DeleteUserPayload
   `,
   inputs: `
+    
+    # Used for the createUser and updateUser mutations.
     input CreateUser {
+
+      # The first name of the User.
       firstName: String!
+
+      # The last name of the User.
       lastName: String!
+
+      # The email address of the User.
       email: String!
+
+      # The role the User is to have. Must be one of 'user', 'manager' or
+      # 'admin'
       role: String!
+
+      # The password that the User is to have.
       password: String!
+
     }
 
+    # Used for the updateUser mutation.
     input UpdateUser {
+
+      # The ID of the User to update.
       id: Int!
+
+      # The information of the User that is to be changed.
       patch: CreateUser
+
     }
 
+    # Used for the deleteUser mutation.
     input DeleteUser {
+
+      # The ID of the User to be deleted.
       id: Int!
+      
     }
   `,
 };
